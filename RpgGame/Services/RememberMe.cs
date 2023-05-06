049594f88cb007f6beb6b2577fcbc05f5dbab530
Ben H <ben@benmakesgames.com>
2023-05-05 21:35:44 -0400

    slightly-better remember me code (but I should probably do away with the whole encryption thing, anyway)

--- RpgGame/Services/RememberMe.cs
+++ RpgGame/Services/RememberMe.cs
@@ -1,6 +1,5 @@
-﻿using Blazored.LocalStorage;
-using Microsoft.EntityFrameworkCore;
-using RpgGame.Database;
+﻿using System.Security.Cryptography;
+using Blazored.LocalStorage;
 using RpgGame.Functions;
 
 namespace RpgGame.Services;
@@ -9,21 +8,18 @@ public sealed class RememberMe
 {
     private const string LocalStorageKey = "remember-me";
     
-    // TODO: before deploying this site for public use, create your own, random 32-byte string:
-    private const string EncryptionKey = "rh622a=TCCE_BH{mxJ@79-=w..Wf[GSW";
-    
     private ILocalStorageService LocalStorage { get; }
-    private IDbContextFactory<RpgGameDatabase> DatabaseFactory { get; }
     private CurrentPlayer CurrentPlayer { get; }
+    
+    private RememberMeConfig Config { get; }
 
     public RememberMe(
-        ILocalStorageService localStorage, IDbContextFactory<RpgGameDatabase> databaseFactory,
-        CurrentPlayer currentPlayer
+        ILocalStorageService localStorage, CurrentPlayer currentPlayer, IConfiguration config
     )
     {
         LocalStorage = localStorage;
-        DatabaseFactory = databaseFactory;
         CurrentPlayer = currentPlayer;
+        Config = config.GetSection("RememberMe").Get<RememberMeConfig>() ?? throw new Exception("RememberMe section is missing from appsettings.");
     }
 
     public async Task LogIn()
@@ -33,17 +29,22 @@ public sealed class RememberMe
         if (data is null)
             return;
 
+        string? password;
+        
         try
         {
-            var password = Security.Decrypt(data.Password, EncryptionKey);
-        
-            await CurrentPlayer.LogInAsync(data.EmailAddress, password);
+            password = Security.Decrypt(data.Password, Config.EncryptionKey);
         }
-        catch(FormatException e)
+        catch(Exception)
         {
             await Forget();
-            Console.WriteLine("Remember Me data was bad. Deleted it.");
+            return;
         }
+        
+        var result = await CurrentPlayer.LogInAsync(data.EmailAddress, password);
+
+        if(!result.Success)
+            await Forget();
     }
     
     public async Task Remember(string emailAddress, string password)
@@ -51,7 +52,7 @@ public sealed class RememberMe
         await LocalStorage.SetItemAsync(LocalStorageKey, new RememberMeData
         {
             EmailAddress = emailAddress,
-            Password = Security.Encrypt(password, EncryptionKey),
+            Password = Security.Encrypt(password, Config.EncryptionKey),
         });
     }
 
@@ -65,4 +66,9 @@ public sealed class RememberMe
         public required string EmailAddress { get; init; }
         public required string Password { get; init; }
     }
+}
+
+public sealed class RememberMeConfig
+{
+    public required string EncryptionKey { get; init; }
 }
